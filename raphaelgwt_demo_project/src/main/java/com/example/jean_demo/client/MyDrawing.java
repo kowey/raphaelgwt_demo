9a86aa5937d3b827d3f634c80d2e69848a57010a
Eric Kow <eric.kow@gmail.com>
2014-01-20 14:37:15 +0000

    Draggable rectangle

--- raphaelgwt_demo_project/src/main/java/com/example/jean_demo/client/MyDrawing.java
+++ raphaelgwt_demo_project/src/main/java/com/example/jean_demo/client/MyDrawing.java
@@ -64,8 +64,10 @@ public class MyDrawing extends Raphael {
     final int circ1_x = 30;
     final int circ1_y = 60;
 
-    final Raphael.Rect box1 = new Raphael.Rect(box1_x, box1_y, 50,  100, 20);
-    final Raphael.Rect box2 = new Raphael.Rect(box2_x, box2_y, 150, 100, 20);
+    final DraggableRect box1 = new DraggableRect(this, 50,  100, 20);
+    final DraggableRect box2 = new DraggableRect(this, 150, 100, 20);
+    //final Raphael.Rect rectB1 = new Raphael.Rect(box1_x, box1_y, 50+5, 100+5, 0);
+    //final Raphael.Rect rectB2 = new Raphael.Rect(box2_x, box2_y, 150+5,  100+5, 0);
 
     PathBuilder pb = new PathBuilder();
     pb.M(box1_x, box2_y)
@@ -91,9 +93,13 @@ public class MyDrawing extends Raphael {
     // add a new circle to the boundary panel and make it draggable
     final DraggableCircle circ0 = new DraggableCircle(this, 20);
     final DraggableCircle circ1 = new DraggableCircle(this, 40);
+    final Circle boundary0 = new Circle(circ0_x, circ0_y, 25);
+    final Circle boundary1 = new Circle(circ1_x, circ1_y, 45);
 
     dragController.makeDraggable(circ0);
     dragController.makeDraggable(circ1);
+    dragController.makeDraggable(box1);
+    dragController.makeDraggable(box2);
 
     circ0.addMouseDownHandler(new MouseDownHandler() {
       @Override
@@ -107,7 +113,8 @@ public class MyDrawing extends Raphael {
     RootPanel rp = RootPanel.get();
     circ0.addToPanel(rp, circ0_x,circ0_y);
     circ1.addToPanel(rp, circ1_x, circ1_y);
-
+    box1.addToPanel(rp, box1_x, box1_y);
+    box2.addToPanel(rp, box2_x, box2_y);
 
 
   }
