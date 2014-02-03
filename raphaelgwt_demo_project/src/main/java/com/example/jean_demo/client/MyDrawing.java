8d4ac529c26156a050cea39a18206298a8ea1706
Eric Kow <eric.kow@gmail.com>
2014-02-03 17:30:16 +0000

    Minimal demo of a template copy workflow

--- raphaelgwt_demo_project/src/main/java/com/example/jean_demo/client/MyDrawing.java
+++ raphaelgwt_demo_project/src/main/java/com/example/jean_demo/client/MyDrawing.java
@@ -16,92 +16,88 @@ package com.example.jean_demo.client;
  * limitations under the License.
  */
 
+import com.google.common.base.Optional;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.event.dom.client.MouseUpEvent;
+import com.google.gwt.event.dom.client.MouseUpHandler;
 import com.google.gwt.user.client.ui.*;
 import com.hydro4ge.raphaelgwt.client.Raphael;
+import lombok.Data;
+import lombok.NonNull;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import java.lang.Math;
+
 
 /**
  * simple drawing demonstration
  */
 public class MyDrawing extends Raphael {
-  /* center point of the drawing */
-  private int cx;
-  private int cy;
-
-  /**
-   * MyDrawing constructor
-   */
-  public MyDrawing(final int width, final int height) {
-    super(width, height);
-    this.cx = width/2;
-    this.cy = height/2;
-  }
-
-  /**
-   * Raphael Text and the Firefox 3.6 SVG implementation do not
-   * work together when the text is appended to the drawing
-   * before the drawing is appended to the document.  Therefore,
-   * we defer the layout to onLoad() here instead of doing it in
-   * the constructor.
-   */
-  @Override
-  public void onLoad() {
-    super.onLoad();
-
-    final int box1_x = (int)Math.round(cx * 0.5);
-    final int box1_y = (int)Math.round(cy * 0.5);
-
-    final int box2_x = (int)Math.round(cx * 0.8);
-    final int box2_y = (int)Math.round(cy * 0.5);
-
-    final int circ0_x = (int)Math.round(cx * 0.5);
-    final int circ0_y = (int)Math.round(cy * 0.5);
-
-    final int circ1_x = 30;
-    final int circ1_y = 60;
-
-    final DraggableRect box1 = new DraggableRect(this, 50,  100, 20);
-    final DraggableRect box2 = new DraggableRect(this, 150, 100, 20);
-
-    box1.attr("fill", "red");
-    box2.attr("fill", "#d1b48c");
-
-    // add a new circle to the boundary panel and make it draggable
-    final DraggableCircle circ0 = new DraggableCircle(this, 20);
-    final DraggableCircle circ1 = new DraggableCircle(this, 40);
-
-    // FIXME: I'm not thrilled about accessing the RootPanel directly here but
-    // I can't seem to find a way around it
-    RootPanel rp = RootPanel.get();
-
-    AbsolutePanel subpanel = new AbsolutePanel();
-    subpanel.setWidth("400px");
-    subpanel.setHeight("400px");
-    rp.add(subpanel, 5, 5);
-
-    circ0.addToPanel(rp, circ0_x, circ0_y);
-    circ1.addToPanel(subpanel, circ1_x, circ1_y);
-
-    circ0.getElement().setId("kowey-box1");
-    circ1.getElement().setId("kowey-box2");
+    /* center point of the drawing */
+    private int cx;
+    private int cy;
 
 
-    List<DraggableShape> shapes = new ArrayList<DraggableShape>();
-    shapes.add(circ0);
-    shapes.add(circ1);
-    for (DraggableShape s : shapes) {
-      s.getElement().setClassName("koweybox");
+    /**
+     * MyDrawing constructor
+     */
+    public MyDrawing(final int width, final int height) {
+        super(width, height);
+        this.cx = width/2;
+        this.cy = height/2;
     }
-    subpanel.getElement().setClassName("koweydrag");
-    subpanel.getElement().setId("kowey-container");
-    gwtjsPlumbDemo();
-  }
 
-  public static native void gwtjsPlumbDemo() /*-{
+
+
+    /**
+     * Raphael Text and the Firefox 3.6 SVG implementation do not
+     * work together when the text is appended to the drawing
+     * before the drawing is appended to the document.  Therefore,
+     * we defer the layout to onLoad() here instead of doing it in
+     * the constructor.
+     */
+    @Override
+    public void onLoad() {
+        super.onLoad();
+
+        final int circ0_x = (int)Math.round(cx * 0.5);
+        final int circ0_y = (int)Math.round(cy * 0.5);
+
+        final int template_x = 400;
+        final int template_y = 100;
+
+        // add a new circle to the boundary panel and make it draggable
+        final DraggableCircle circ0 = new DraggableCircle(this, 20);
+
+        final Concept conceptTemplate = new Concept("template");
+        conceptTemplate.setLabel(Optional.of("hello"));
+        conceptTemplate.getElement().getStyle().setVisibility(Style.Visibility.HIDDEN);
+
+        // FIXME: I'm not thrilled about accessing the RootPanel directly here but
+        // I can't seem to find a way around it
+        RootPanel rp = RootPanel.get();
+
+        rp.add(conceptTemplate, template_x, template_y);
+
+        final Concept concept = conceptTemplate.copyTemplate(rp, "concept", 0);
+
+        circ0.addToPanel(rp, circ0_x, circ0_y);
+        circ0.getElement().setId("kowey-box1");
+
+        List<DraggableShape> shapes = new ArrayList<DraggableShape>();
+        shapes.add(circ0);
+        for (DraggableShape s : shapes) {
+            s.getElement().setClassName("koweybox");
+        }
+
+        gwtjsPlumbDemo();
+    }
+
+    public static native void gwtjsPlumbDemo() /*-{
                 $wnd.gwtjsplumbdemo();
-
         }-*/;
 
 }
