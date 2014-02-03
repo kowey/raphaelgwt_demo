07ce218b05bbf8e8bf1b7c0fe6d2ede3cd10af9b
Eric Kow <eric.kow@gmail.com>
2014-02-03 09:42:16 +0000

    pure-js: demonstrate nested element dragging

--- pure-js/dynamic/demo.js
+++ pure-js/dynamic/demo.js
@@ -1,101 +1,27 @@
 jsPlumb.ready(function() {
 
-	var sourceAnchors = [[0.2, 0, 0, -1, 0, 0, "foo"], [1, 0.2, 1, 0, 0, 0, "bar"], [0.8, 1, 0, 1, 0, 0, "baz"], [0, 0.8, -1, 0, 0, 0, "qux"] ],
-		targetAnchors = [[0.6, 0, 0, -1], [1, 0.6, 1, 0], [0.4, 1, 0, 1], [0, 0.4, -1, 0] ],
+    var fillColor = "gray";
+    jsPlumb.Defaults.Connector = [ "Bezier", { curviness:25 } ];
+    jsPlumb.Defaults.DragOptions = { cursor: "pointer", zIndex:2000 };
+    jsPlumb.Defaults.PaintStyle = { strokeStyle:"gray", lineWidth:2 };
+    jsPlumb.Defaults.EndpointStyle = { radius:3, fillStyle:"gray" };
+    jsPlumb.Defaults.Anchors =  [ "AutoDefault", "AutoDefault" ];
+    jsPlumb.Defaults.ConnectionsDetachable = false;
+    jsPlumb.Defaults.Container = "dynamic-demo";
 
-		exampleColor = '#00f',
-		exampleDropOptions = {
-				tolerance:'touch',
-				hoverClass:'dropHover',
-				activeClass:'dragActive'
-		}, 
-		connector = [ "Bezier", { cssClass:"connectorClass", hoverClass:"connectorHoverClass" } ],
-		connectorStyle = {
-			gradient:{stops:[[0, exampleColor], [0.5, '#09098e'], [1, exampleColor]]},
-			lineWidth:5,
-			strokeStyle:exampleColor
-		},
-		hoverStyle = {
-			strokeStyle:"#449999"
-		},
-		overlays = [ ["Diamond", { fillStyle:"#09098e", width:15, length:15 } ] ],
-		endpoint = ["Dot", { cssClass:"endpointClass", radius:10, hoverClass:"endpointHoverClass" } ],
-		endpointStyle = { fillStyle:exampleColor },
-		anEndpoint = {
-			endpoint:endpoint,
-			paintStyle:endpointStyle,
-			hoverPaintStyle:{ fillStyle:"#449999" },
-			isSource:true, 
-			isTarget:true, 
-			maxConnections:-1, 
-			connector:connector,
-			connectorStyle:connectorStyle,
-			connectorHoverStyle:hoverStyle,
-			connectorOverlays:overlays
-		};
-	
-	var instance = jsPlumb.getInstance({
-		DragOptions : { cursor: 'pointer', zIndex:2000 },
-		Container:"dynamic-demo"
-	});
 
-	// suspend drawing and initialise.
-	instance.doWhileSuspended(function() {
+    // declare some common values:
+    var arrowCommon = { foldback:0.3, fillStyle:fillColor, width:8 };
+    // use three-arg spec to create two different arrows with the common values:
+    var overlays = [
+        [ "Arrow", { location:1 }, arrowCommon,
+         ]
+    ];
 
-		var connections = {
-			"dynamicWindow1":["dynamicWindow4"],
-			"dynamicWindow3":["dynamicWindow1"],
-			"dynamicWindow5":["dynamicWindow3"],
-			"dynamicWindow6":["dynamicWindow5"],
-			"dynamicWindow2":["dynamicWindow6"],
-			"dynamicWindow4":["dynamicWindow2"]
-			
-		},
-		endpoints = {},			
-		// ask jsPlumb for a selector for the window class
-		divsWithWindowClass = jsPlumb.getSelector(".dynamic-demo .window");
-		
-		// add endpoints to all of these - one for source, and one for target, configured so they don't sit
-		// on top of each other.
-		for (var i = 0 ; i < divsWithWindowClass.length; i++) {
-			var id = instance.getId(divsWithWindowClass[i]);
-			endpoints[id] = [
-				// note the three-arg version of addEndpoint; lets you re-use some common settings easily.
-				instance.addEndpoint(id, anEndpoint, {anchor:sourceAnchors}),
-				instance.addEndpoint(id, anEndpoint, {anchor:targetAnchors})
-			];
-		}
-		// then connect everything using the connections map declared above.
-		for (var e in endpoints) {
-			if (connections[e]) {
-				for (var j = 0; j < connections[e].length; j++) {					
-					instance.connect({
-						source:endpoints[e][0],
-						target:endpoints[connections[e][j]][1]
-					});						
-				}
-			}	
-		}
-		
-		// bind click listener; delete connections on click			
-		instance.bind("click", function(conn) {
-			instance.detach(conn);
-		});
-		
-		// bind beforeDetach interceptor: will be fired when the click handler above calls detach, and the user
-		// will be prompted to confirm deletion.
-		instance.bind("beforeDetach", function(conn) {
-			return confirm("Delete connection?");
-		});
-		
-		//
-		// configure ".window" to be draggable. 'getSelector' is a jsPlumb convenience method that allows you to
-		// write library-agnostic selectors; you could use your library's selector instead, eg.
-		//
-		// $(".window")  		jquery
-		// $$(".window") 		mootools
-		// Y.all(".window")		yui3
-		//
-		instance.draggable(divsWithWindowClass);	
-	});
+    jsPlumb.draggable(jsPlumb.getSelector(".window"));
+    jsPlumb.draggable(jsPlumb.getSelector(".subwindow"),{containment:"#dynamicWindow1"});
+    jsPlumb.connect({source:"dynamicWindow1", target:"dynamicWindow4", overlays:overlays});
+    jsPlumb.connect({source:"dynamicWindow1_1", target:"dynamicWindow3", overlays:overlays});
+    jsPlumb.connect({source:"dynamicWindow2", target:"dynamicWindow1_1", overlays:overlays});
+
 });
